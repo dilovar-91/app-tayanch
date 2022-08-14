c0d57e2b344dfd78b4b266d7073526e98231a5da
Dilovar <dilovar09@gmail.com>
2022-08-14 15:18:52 +0300

    order BY desc created_at

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function index(): JsonResponse
     {
-        $posts = Post::get();
+        $posts = Post::orderby('created_at', 'DESC')->get();
         return response()->json($posts);
     }
 }
