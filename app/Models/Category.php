0883c3cc955862a5f44a3c398b4ab51652524e12
Dilovar <dilovar09@gmail.com>
2022-08-15 20:02:56 +0300

    cast description

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $hidden = ['seo_title','seo_description','seo_keywords'];
+
     protected $casts = ['name'=>'array', 'description'=>'array'];
 }
